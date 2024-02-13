c6722c1f394a024c4333492e22431b2d42e96e9f
jochoaAvant <111658059+jochoaAvant@users.noreply.github.com>
2024-02-13 08:30:46 -0500

    working script - prior to ability to ignore errors

--- TestScript.py
+++ TestScript.py
@@ -1,3 +1,8 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+
+
 def rename_and_drop_columns(input_df, column_mapping_df):
     # Create a dictionary from the column_mapping dataframe
     column_mapping_dict = column_mapping_df.set_index(
@@ -18,14 +23,14 @@ def rename_and_drop_columns(input_df, column_mapping_df):
 
 def basic_conditioning(df1, df2):
     # Remove 'Id' column and 'VIA' Column if any exists
-    if (any(column_name == 'id' for column_name in df2.columns)):
+    if any(column_name == 'id' for column_name in df2.columns):
         df2.drop(['id'], axis=1, inplace=True)
-    if (any(column_name == 'VIA' for column_name in df1.columns)):
+    if any(column_name == 'VIA' for column_name in df1.columns):
         df1.drop(['VIA'], axis=1, inplace=True)
     return df1, df2
 
 
-def basic_validation(df1, df2, result_file, ignore_rows=0):
+def basic_validation(df1, df2, result_file, _ignore_rows=0):
     # Check if the dataframes have the same number of rows and columns
     if df1.shape == df2.shape:
         result_file.write(
@@ -42,8 +47,9 @@ def basic_validation(df1, df2, result_file, ignore_rows=0):
             f"The manual dataframe has {df1.shape[0]} rows and {df1.shape[1]} columns.\n")
         print(
             f"The automated dataframe has {df2.shape[0]} rows and {df2.shape[1]} columns.\n")
-        if (df1.shape[1] != df2.shape[1]):
-            if (ignore_rows == 1):
+        # Check if the number of columns is the same.
+        if df1.shape[1] == df2.shape[1]:
+            if _ignore_rows == 1:
                 result_file.write(
                     "Ignoring the number of rows in the comparison.\n")
                 print("Ignoring the number of rows in the comparison.")
@@ -73,15 +79,6 @@ def basic_validation(df1, df2, result_file, ignore_rows=0):
                                          'DataTypeAutomated': df2[column].dtype}])
                 comparison_df = pd.concat([comparison_df,
                                           new_row], ignore_index=True)
-            else:
-                result_file.write(
-                    f"The column '{column}' has same data types in the dataframes.\n")
-                print(
-                    f"The column '{column}' has same data types in the dataframes.")
-                new_row = pd.DataFrame([{'Column': column, 'Same DataType': 'yes',
-                                       'DataTypeManual': df1[column].dtype, 'DataTypeAutomated': df2[column].dtype}])
-                comparison_df = pd.concat([comparison_df,
-                                          new_row], ignore_index=True)
         if different_types == 1:
             error = 5
             return error, comparison_df
@@ -105,9 +102,9 @@ def sort_rows(unsorted_df, columns_to_sort):
         # Sort the dataframe
         df_sorted = unsorted_df.sort_values(
             list(columns_to_sort)).reset_index(drop=True)
-        return df_sorted
+        return (0, df_sorted)
     else:
-        return "One or more columns in columns_to_sort do not exist in the dataframe."
+        return (8, df_sorted)
 
 
 def compare_df(df1, df2, result_file):
@@ -138,36 +135,32 @@ def compare_df(df1, df2, result_file):
         columns={'index': 'original_row_number', 'self': 'manual', 'other': 'automated'})
 
     if not diff.empty:
-        result_file.write(f"The dataframes have differences.\n")
+        result_file.write("The dataframes have differences.\n")
         return 7, diff
     else:
-        result_file.write(
-            f"The dataframes have the same columns and values.\n")
-        return 0, "The dataframes have the same columns and values."
+        result_file.write("The dataframes have the same columns and values.\n")
+        return 0, diff
 
 
-def main(vendor, filename, new_file, ignore_rows, error_codes):
-
-    import pandas as pd
-    import numpy as np
-    from pathlib import Path
-
+def main(_vendor, _filename, _new_file, _ignore_rows, _error_codes):
+    # Set the vendor and filename
     # Set the filenames for the manual and automated files
-    filenamea = filename + 'a.xlsx'
-    filenamem = filename + 'm.xlsx'
+    filenamea = _filename + 'a.xlsx'
+    filenamem = _filename + 'm.xlsx'
 
     # Set the columns to sort the dataframes on
     columns_to_sort = ('Account', 'Product name',
                        'Net billed', 'Gross commission')
 
     # establish file paths for input and output files
-    manual_url = Path(vendor, "test_files", "rpm_files_manual", filenamem)
-    automated_url = Path(vendor, "test_files",
+    manual_url = Path(_vendor, "test_files", "rpm_files_manual", filenamem)
+    automated_url = Path(_vendor, "test_files",
                          "rpm_files_automation", filenamea)
-    output_url = Path(vendor, "test_files", "output",
-                      filename + '_output.xlsx')
-    column_mapping_url = Path(vendor, "test_files", "column_mapping.xlsx")
-    test_results_url = Path(vendor, "test_files", "output", "test_results.txt")
+    output_url = Path(_vendor, "test_files", "output",
+                      _filename + '_output.xlsx')
+    column_mapping_url = Path(_vendor, "test_files", "column_mapping.xlsx")
+    test_results_url = Path(_vendor, "test_files",
+                            "output", "test_results.txt")
 
     # read in the input files
     try:
@@ -186,31 +179,32 @@ def main(vendor, filename, new_file, ignore_rows, error_codes):
         print("ERROR: opening mapping file")
 
     try:
-        if (new_file):
+        if (_new_file):
             test_results = open(test_results_url, 'wt')
-            test_results.write(f"Test results for '{vendor}'\n")
+            test_results.write(f"Test results for '{_vendor}'\n")
         else:
             test_results = open(test_results_url, 'at')
     except:
         print("ERROR: opening test results file")
 
     test_results.write(
-        f"\n\n{'-'*37}\nTest results for '{vendor}' - '{filename}' \n")
+        f"\n\n{'-'*37}\nTest results for '{_vendor}' - '{_filename}' \n")
 
     # delete unwanted columns
     manual, automated = basic_conditioning(manual, automated)
 
     # run basic validation on the dataframes
     error, output_df = basic_validation(
-        manual, automated, test_results, ignore_rows=ignore_rows)
+        manual, automated, test_results, _ignore_rows=_ignore_rows)
 
     match error:
         case 1 | 2 | 4 | 5:
-            print(error_codes[error])
+            print(_error_codes[error])
             print(output_df)
             print("ERROR: Dataframes failed basic validation")
             test_results.write("ERROR: Dataframes failed basic validation")
-            test_results.write(error_codes[error])
+            test_results.write(f'{error_codes[error]} \n')
+            test_results.write(output_df.to_string())
 
         case 3 | 6:
             # Remap columns in manual and automated based on the mapping dataframe
@@ -219,39 +213,52 @@ def main(vendor, filename, new_file, ignore_rows, error_codes):
             automated = rename_and_drop_columns(automated, column_mapping)
 
             # Sort columns on both dataframes to compare
-            manual = sort_rows(manual, columns_to_sort)
-            automated = sort_rows(automated, columns_to_sort)
+            manual_sort_error, manual = sort_rows(manual, columns_to_sort)
+            automated_sort_error, automated = sort_rows(
+                automated, columns_to_sort)
 
             # Compare the sorted dataframes
-            error, df_differences = compare_df(
-                manual, automated, result_file=test_results)
-            print(df_differences)
-
-            # Produce output files
-            with pd.ExcelWriter(output_url) as writer:
-                manual.to_excel(writer, sheet_name='manual', index=False)
-                automated.to_excel(writer, sheet_name='automated', index=False)
-            #     if isinstance(df_differences, pd.DataFrame):
-            #         df_differences.to_excel(writer, sheet_name='differences', index=False)
-            #     else:
-            #         pd.DataFrame([df_differences]).to_excel(writer, sheet_name='differences', index=False)
+            if manual_sort_error == 0 and automated_sort_error == 0:
+                differences_error, df_differences = compare_df(
+                    manual, automated, result_file=test_results)
+                print(_error_codes[differences_error])
+                print(df_differences)
+                test_results.write(
+                    f'{_error_codes[differences_error]} \n Differences: \n')
+                test_results.write(df_differences.to_string())
+                # Produce output files
+                with pd.ExcelWriter(output_url) as writer:
+                    manual.to_excel(writer, sheet_name='manual', index=False)
+                    automated.to_excel(
+                        writer, sheet_name='automated', index=False)
+                #     if isinstance(df_differences, pd.DataFrame):
+                #         df_differences.to_excel(writer, sheet_name='differences', index=False)
+                #     else:
+                #         pd.DataFrame([df_differences]).to_excel(writer,
+                #               sheet_name='differences', index=False)
+            elif manual_sort_error == 8:
+                print(_error_codes[manual_sort_error])
+                test_results.write(_error_codes[manual_sort_error])
+            elif automated_sort_error == 8:
+                print(_error_codes[automated_sort_error])
+                test_results.write(_error_codes[automated_sort_error])
+            else:
+                print('Unknown error while sorting dataframes')
+                test_results.write('Unknown error while sorting dataframes')
     test_results.close()
 
 
 # Run the main function
 if __name__ == "__main__":
-    import pandas as pd
-    import numpy as np
-    from pathlib import Path
     # vendor = input ("Vendor: ")
-    vendor = 'AllStream'
+    vendor = 'Nitel'
 
     # filename = input ("Filename (mmm-yy): ")
-    filename = 'aug-23'
+    filename = 'dec-23'
 
     # Set variables
     # Set variable to 1 to start a new results file, or to 0 to append to an existing file
-    new_file = 1
+    new_file = 0
     ignore_rows = 1    # Set variable to 1 to ignore the number of rows in the comparison, or to 0 to compare the number of rows
 
     # Set dictionary of error codes
@@ -263,7 +270,8 @@ if __name__ == "__main__":
         4: 'The dataframes have the same number of columns, different columns names.',
         5: 'The dataframe have the same columns, but different data types.',
         6: 'The dataframes have the same columns and data types.',
-        7: 'The dataframes have different values.'
+        7: 'The dataframes have different values.',
+        8: 'One or more columns in columns_to_sort do not exist in the dataframe.'
     }
-    main(vendor=vendor, filename=filename, new_file=new_file, ignore_rows=ignore_rows,
-         error_codes=error_codes)
+    main(_vendor=vendor, _filename=filename, _new_file=new_file, _ignore_rows=ignore_rows,
+         _error_codes=error_codes)
